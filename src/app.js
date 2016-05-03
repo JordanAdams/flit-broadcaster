e41b23801d59ce486db02a08594627662449a125
Jordan Adams <hi@jordanadams.co.uk>
2016-05-03 08:50:11 +0100

    Several changes; Redis channels & performance
    - Changes Redis channels
    - Improves connection management
    - Switches to using two Redis instances (subscriber & retriever)

--- src/app.js
+++ src/app.js
@@ -1,25 +1,92 @@
+import http from 'http';
+import socketIO from 'socket.io';
+import chunk from 'lodash.chunk';
 import redis from './services/redis';
-var server = require('http').createServer()
-var io = require('socket.io')(server);
 
+const server = http.createServer()
+const io = socketIO(server)
+
+const redisClient = redis.createClient();
+const redisSubscriber = redis.createClient();
+
+let sockets = [];
 io.on('connection', (socket) => {
-  redis.on('message', (channel, message) => {
-    switch (channel) {
-      case 'positive_changed':
-        socket.emit(channel, parseInt(message, 10));
-        break;
+  sockets = sockets.concat(socket)
 
-      case 'negative_changed':
-        socket.emit(channel, parseInt(message, 10));
-        break;
+  redisClient.getAsync('positive')
+    .then((positive) => {
+      socket.emit('positive', parseInt(positive, 10));
+    })
+    .then(() => redisClient.getAsync('negative'))
+    .then((negative) => {
+      socket.emit('negative', parseInt(negative, 10));
+    })
+    .then(() => redisClient.zrangebyscoreAsync('positive_words', '-inf', '+inf', 'WITHSCORES'))
+    .then((words) => {
+      const payload = chunk(words, 2).map((word) => {
+        return {
+          word: word[0],
+          value: parseInt(word[1])
+        };
+      });
 
-      default: break;
-    }
+      socket.emit('positive_words', payload);
+    })
+    .then(() => redisClient.zrangebyscoreAsync('negative_words', '-inf', '+inf', 'WITHSCORES'))
+    .then((words) => {
+      const payload = chunk(words, 2).map((word) => {
+        return {
+          word: word[0],
+          value: parseInt(word[1])
+        };
+      });
+
+      socket.emit('negative_words', payload);
+    })
+    .catch((err) => {
+      throw err
+    });
+
+  socket.on('disconnect', () => {
+    sockets = sockets.filter(s => s !== socket);
   });
 });
 
-redis.subscribe('positive_changed');
-redis.subscribe('negative_changed');
+
+redisSubscriber.on('message', (channel, message) => {
+  switch (channel) {
+    case 'positive':
+      sockets.forEach(socket => {
+        socket.emit(channel, parseInt(message, 10))
+      });
+      break;
+
+    case 'negative':
+      sockets.forEach(socket => {
+        socket.emit(channel, parseInt(message, 10))
+      });
+      break;
+
+    case 'positive_words':
+      sockets.forEach(socket => {
+        socket.emit(channel, JSON.parse(message))
+      });
+      break;
+
+    case 'negative_words':
+      sockets.forEach(socket => {
+        socket.emit(channel, JSON.parse(message))
+      });
+      break;
+
+    default: break;
+  }
+});
+
+redisSubscriber.subscribe('positive');
+redisSubscriber.subscribe('negative');
+redisSubscriber.subscribe('positive_word_changed');
+redisSubscriber.subscribe('negative_word_changed');
 
 const port = process.env.PORT || 3000;
 server.listen(port);
--- src/services/redis.js
+++ src/services/redis.js
@@ -4,13 +4,4 @@ import redis from 'redis';
 bluebird.promisifyAll(redis.RedisClient.prototype);
 bluebird.promisifyAll(redis.Multi.prototype);
 
-const client = redis.createClient();
-
-client.on('error', error => {
-  throw error;
-});
-
-client.on('connect', () => console.log('Redis: Connected'));
-client.on('ready', () => console.log('Redis: Ready'));
-
-export default client;
+export default redis;
